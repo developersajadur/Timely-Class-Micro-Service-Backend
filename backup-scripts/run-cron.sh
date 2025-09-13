b71eda8794354726a92fd51b6d12de483e135675
Sajadur Rahman <itzmesojib@gmail.com>
2025-09-13 10:09:35 -0700

    add database backup on google drive by using cron and rclone

--- backup-scripts/run-cron.sh
+++ backup-scripts/run-cron.sh
@@ -13,8 +13,8 @@ mkdir -p /var/spool/cron/crontabs
 touch /var/spool/cron/crontabs/root
 chmod 600 /var/spool/cron/crontabs/root
 
-# Cron: backup every hour
-echo "* * * * * export PGPASSWORD=$PGPASSWORD && pg_dump -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB > /backups/backup-\$(date +\%F-%H%M%S).sql && rclone copy /backups/backup-\$(date +\%F-%H%M%S).sql gdrive:/TimelyClassBackups --progress && find /backups -type f -mtime +5 -delete" > /var/spool/cron/crontabs/root
+#  add cron for daily backup at 2AM
+echo "0 2 * * * export PGPASSWORD=$PGPASSWORD && pg_dump -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_DB > /backups/backup-\$(date +\%F-%H%M%S).sql && rclone copy /backups/backup-\$(date +\%F-%H%M%S).sql gdrive:/TimelyClassBackups --progress && find /backups -type f -mtime +5 -delete" > /var/spool/cron/crontabs/root
 
 # Run first backup immediately
 BACKUP_FILE="/backups/backup-$(date +%F-%H%M%S).sql"
